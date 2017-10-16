264e056100abedac37f999f28de5f519053ee37c
arve0 <arve.seljebu@gmail.com>
2017-10-16 23:55:42 +0200

    use pie chart for relative size

---
+++ src/Dep.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class=dep>
+    <div class="chart">
+      <pie-chart
+        :ratio=ratio
+        :label-small=pretty(size)
+      />
+    </div>
+    <div class="details">
+      <h1 class=link @click="$emit('goto', pkg.name)">{{pkg.name}}</h1>
+      <table>
+        <tr>
+          <th>Version</th>
+          <td>{{pkg.version}}</td>
+        </tr>
+        <tr>
+          <th>Tarball size</th>
+          <td>{{pretty(pkg.tarballSize)}}</td><!-- TODO: percent -->
+        </tr>
+        <tr>
+          <th>Total size</th>
+          <td>{{pretty(pkg.size)}}</td>
+        </tr>
+        <tr v-if="pkg === parent">
+          <th>Direct dependencies</th>
+          <td>{{pkg.dependencies.length}}</td>
+        </tr>
+        <tr>
+          <th>Total dependencies</th>
+          <td>{{pkg.totalDependencies}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import prefix from 'si-prefix'
+import PieChart from 'vue-pie-chart'
+
+export default {
+  props: ['pkg', 'parent'],
+  components: {
+    'pie-chart': PieChart
+  },
+  computed: {
+    size: function () {
+      return this.pkg !== this.parent
+        ? this.pkg.size
+        : this.pkg.tarballSize
+    },
+    ratio: function () {
+      return this.size / this.parent.size
+    }
+  },
+  methods: {
+    pretty: function (size) {
+      let converted = size === 0
+        ? [0, 'B']  // avoid NaN, https://github.com/mal/si-prefix/issues/1
+        : prefix.byte.convert(size)
+      let output = converted[0].toFixed(1) + ' ' + converted[1]
+      return output
+    },
+  }
+}
+</script>
+
+<style scoped>
+.dep {
+  border-bottom: 2px solid;
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+.chart {
+  width: 30%;
+  max-width: 200px;
+  padding: 2em;
+  display: inline-block;
+}
+.details {
+  display: inline-block;
+  vertical-align: top;
+}
+table {
+  border-collapse: collapse;
+  margin-bottom: 1em;
+}
+th, td {
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: black;
+}
+th {
+  text-align: left;
+  padding-right: 2em;
+}
+td {
+  text-align: right;
+}
+.link {
+  cursor: pointer;
+}
+.small {
+  font-size: 80%;
+}
+.tiny {
+  font-size: 60%;
+}
+
+</style>
--- src/View.vue
+++ src/View.vue
@@ -1,107 +1,36 @@
 <template>
   <div id=view  v-if="pkg">
-    <h1>{{pkgName}}</h1>
-    <table>
-      <tr>
-        <th>Version</th>
-        <td>{{pkg.version}}</td>
-      </tr>
-      <tr>
-        <th>Tarball size</th>
-        <td>{{pretty(pkg.tarballSize)}}</td>
-      </tr>
-      <tr>
-        <th>Total size</th>
-        <td>{{pkg.prettySize}}</td>
-      </tr>
-      <tr>
-        <th>Direct dependencies</th>
-        <td>{{directDependencies}}</td>
-      </tr>
-      <tr>
-        <th>Total dependencies</th>
-        <td>{{pkg.totalDependencies}}</td>
-      </tr>
-    </table>
-    <div class=deps v-if="deps.first.length">
-      <div
-        class=dep
-        v-for="dep in deps.first"
-        :key="dep.name"
-        :style="'height:' + dep.percent + '%'"
-      >
-        <span class=link @click="goTo(dep)">{{dep.name}}
-          <span class="tiny">{{dep.version}}</span>
-        </span>
-        <span class=small>{{dep.prettySize}} ({{dep.percent.toFixed(0)}}%)</span>
-      </div>
-      <div
-        class=dep
-        v-if="deps.packed.length"
-        :style="'height:' + deps.packPercent + '%'"
-      >
-        <span>{{deps.packed.map(d => d.name).join(', ')}}</span>
-        <span class=small>{{pretty(deps.packSize)}} ({{deps.packPercent.toFixed(0)}}%)</span>
-      </div>
-    </div>
+    <dep-component :pkg=pkg :parent=pkg />
+    <h1>Dependencies</h1>
+    <dep-component
+      v-for="dep in deps"
+      :key="dep.name"
+      :pkg=dep
+      :parent=pkg
+      @goto="goTo"
+    />
   </div>
 </template>
 
 <script>
-import prefix from 'si-prefix'
+import Dep from './Dep.vue'
 
 export default {
   props: ['pkgName'],
+  components: {
+    'dep-component': Dep
+  },
   data: function () {
     return {
       pkg: null
     }
   },
   computed: {
-    directDependencies: function () {
-      return this.pkg && this.pkg.dependencies.length
-    },
-    sortedDeps: function () {
+    deps: function () {
       return this.pkg && this.pkg.dependencies.sort((a, b) => b.size - a.size)
     },
-    deps: function () {
-      if (!this.pkg) {
-        return
-      }
-      let height = window.innerHeight
-      let totalSize = this.sortedDeps.reduce((s, d) => s + d.size, 0)
-      let currentPercent = 0
-      return this.sortedDeps.map((d, i) => {
-        d.percent = d.size / totalSize * 100
-        currentPercent += d.percent
-        if (i > 0 && currentPercent > 90 || d.percent < 5) {
-          d.pack = true
-        }
-        return d
-      }).reduce((acc, d) => {
-        if (d.pack) {
-          acc.packed.push(d)
-          acc.packPercent += d.percent
-          acc.packSize += d.size
-        } else {
-          acc.first.push(d)
-        }
-        return acc
-      }, { first: [], packed: [], packPercent: 0, packSize: 0 })
-    }
-    // portrait: function () {
-    //   let orientation = screen.orientation || screen.mozOrientation || screen.msOrientation;
-    //   return orientation.slice(0, 8) === 'portrait'
-    // },
   },
   methods: {
-    pretty: function (size) {
-      let converted = size === 0
-        ? [0, 'B']  // avoid NaN, https://github.com/mal/si-prefix/issues/1
-        : prefix.byte.convert(size)
-      let output = converted[0].toFixed(1) + ' ' + converted[1]
-      return output
-    },
     getPkg: function () {
       let uri = `https://api.seljebu.no/download-size/${this.pkgName}`
       fetch(uri).then(res => {
@@ -114,9 +43,9 @@ export default {
         this.pkg = res
       })
     },
-    goTo: function (pkg) {
+    goTo: function (pkgName) {
       this.pkg = null
-      this.$emit('path', `/${pkg.name}`)
+      this.$emit('path', `/${pkgName}`)
     },
   },
   created: function () {
@@ -137,51 +66,5 @@ export default {
 }
 h1 {
   margin-bottom:0;
-}
-table {
-  border-collapse: collapse;
-  margin-bottom: 1em;
-}
-th, td {
-  border-bottom-width: 2px;
-  border-bottom-style: solid;
-  border-bottom-color: black;
-}
-th {
-  text-align: left;
-  padding-right: 2em;
-}
-td {
-  text-align: right;
-}
-.deps {
-  height: 100vh;
-}
-.dep {
-  box-sizing: border-box;
-  border-style: solid;
-  border-width: 1px;
-  border-bottom-width: 0;
-  border-color: black;
-  padding: 0.5em;
-  border-collapse:collapse;
-  text-align: center;
-  display:flex;
-  flex-direction:column;
-  align-items: center;
-  justify-content: center;
-  font-size: 120%;
-}
-.dep:last-child {
-  border-bottom-width: 1px;
-}
-.link {
-  cursor: pointer;
-}
-.small {
-  font-size: 80%;
-}
-.tiny {
-  font-size: 60%;
 }
 </style>
